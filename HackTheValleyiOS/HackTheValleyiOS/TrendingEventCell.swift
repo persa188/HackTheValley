27f9d08c81ac8e772e2be47b7efbd87bc34594cd
Brandon Mowat <brandonmowat11@gmail.com>
2017-01-07 17:46:09 -0500

    Added custom table view cells

---
+++ HackTheValleyiOS/HackTheValleyiOS/TrendingEventCell.swift
@@ -0,0 +1,24 @@
+//
+//  TrendingEventCell.swift
+//  HackTheValleyiOS
+//
+//  Created by Brandon Mowat on 2017-01-07.
+//  Copyright © 2017 Brandon Mowat. All rights reserved.
+//
+
+import UIKit
+
+class TrendingEventCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
